4c682bae5c3a5906080f5b0caf1cc44aee698c24
Trejo <jtrejom003@gmail.com>
2021-10-29 14:57:10 -0500

    v2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyecto-consolecode-frontend'`, () => {
+  it(`should have as title 'proyecto-console-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto-consolecode-frontend');
+    expect(app.title).toEqual('proyecto-console-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-consolecode-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-console-frontend app is running!');
   });
 });
